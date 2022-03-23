01656c5a07a7a0f06b5351d9e9ca8b88dd6299c2
VanSy <vansy0975316291@gmail.com>
2022-03-23 16:36:39 +0700

    Calculator-MVVM

---
+++ Calculator/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+using Calculator.Commands;
+using Calculator.Models;
+
+namespace Calculator.ViewModels
+{
+    public class CalculatorViewModel : ViewModelBase
+    {
+
+        #region Private members
+
+        private Models.CalculationModel calculation;
+
+        private DelegateCommand<string> digitButtonPressCommand;
+        private DelegateCommand<string> operationButtonPressCommand;
+        
+        private string lastOperation;
+        private bool newDisplayRequired = false;
+        private string fullExpression;
+        private string display;
+
+        #endregion
+
+        #region Constructor
+
+        public CalculatorViewModel()
+        {
+            this.calculation = new CalculationModel();
+            this.display = "0";
+            this.FirstOperand = string.Empty;
+            this.SecondOperand = string.Empty;
+            this.Operation = string.Empty;
+            this.lastOperation = string.Empty;
+            this.fullExpression = string.Empty;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public string FirstOperand
+        {
+            get { return calculation.FirstOperand; }
+            set { calculation.FirstOperand = value; }
+        }
+
+        public string SecondOperand
+        {
+            get { return calculation.SecondOperand; }
+            set { calculation.SecondOperand = value; }
+        }
+
+        public string Operation
+        {
+            get { return calculation.Operation; }
+            set { calculation.Operation = value; }
+        }
+
+        public string LastOperation
+        {
+            get { return lastOperation; }
+            set { lastOperation = value; }
+        }
+
+        public string Result
+        {
+            get { return calculation.Result; }
+        }
+
+        public string Display
+        {
+            get { return display; }
+            set
+            {
+                display = value;
+                OnPropertyChanged("Display");
+            }
+        }
+
+        public string FullExpression
+        {
+            get { return fullExpression; }
+            set
+            {
+                fullExpression = value;
+                OnPropertyChanged("FullExpression");
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand OperationButtonPressCommand
+        {
+            get
+            {
+                if (operationButtonPressCommand == null)
+                {
+                    operationButtonPressCommand = new DelegateCommand<string>(
+                        OperationButtonPress, CanOperationButtonPress);
+                }
+                return operationButtonPressCommand;
+            }
+        }
+
+        private static bool CanOperationButtonPress(string number)
+        {
+            return true;
+        }
+        
+        public ICommand DigitButtonPressCommand
+        {
+            get
+            {
+                if (digitButtonPressCommand == null)
+                {
+                    digitButtonPressCommand = new DelegateCommand<string>(
+                        DigitButtonPress, CanDigitButtonPress);
+                }
+                return digitButtonPressCommand;
+            }
+        }
+
+        private static bool CanDigitButtonPress(string button)
+        {
+            return true;
+        }
+
+        #region Display input
+        //deals with button inputs and sorts out the display accordingly        
+
+        public void DigitButtonPress(string button)
+        {
+            switch (button)
+            {
+                case ".":
+                    if (newDisplayRequired)
+                    {
+                        Display = "0.";
+                    }
+                    else
+                    {
+                        if (!display.Contains("."))
+                        {
+                            Display = display + ".";
+                        }
+                    }
+                    break;
+                default:
+                    if (display == "0" || newDisplayRequired)
+                        Display = button;
+                    else
+                        Display = display + button;
+                    break;
+            }
+            newDisplayRequired = false;
+        }
+        #endregion
+
+
+        #region Calculate and display result
+
+        //for operations with 2 operands
+        public void OperationButtonPress(string operation)
+        {
+            try
+            {
+                if (FirstOperand == string.Empty || LastOperation == "=")
+                {
+                    FirstOperand = display;
+                    LastOperation = operation;
+                }
+                else
+                {
+                    SecondOperand = display;
+                    Operation = lastOperation;
+                    calculation.CalculateResult();
+
+                    FullExpression = Math.Round(Convert.ToDouble(FirstOperand), 10) + " " + Operation + " " 
+                                    + Math.Round(Convert.ToDouble(SecondOperand), 10) + " = "
+                                    + Math.Round(Convert.ToDouble(Result), 10);
+
+                    LastOperation = operation;
+                    Display = Result;
+                    FirstOperand = display;
+                }
+                newDisplayRequired = true;
+            }
+            catch(Exception ex)
+            {
+                Display = Result == string.Empty ? "Error - see event log" : Result;
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
